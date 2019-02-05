af1ce9625ca57588f8937afaaff6dcc3af883ce8
Scott Burton <burtons@byui.edu>
2019-02-05 11:28:13 -0700

    regular db connection

--- web/05-movies.php
+++ web/05-movies.php
@@ -3,7 +3,16 @@
 try
 {
   $dbUrl = getenv('DATABASE_URL');
-  $db = new PDO($dbUrl);
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
