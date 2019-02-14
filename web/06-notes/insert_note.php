71edd07ed921bc6fa36a6464ff273c15e238b541
Scott Burton <burtons@byui.edu>
2019-02-14 11:12:47 -0700

    update

--- web/06-notes/insert_note.php
+++ web/06-notes/insert_note.php
@@ -6,4 +6,17 @@ $content = htmlspecialchars($_POST['content']);
 
 echo "Trying to insert: courseid: $course_id, date: $date, content: $content";
 
+require_once("dbconnect.php");
+$db = get_db();
+
+// Get the Course from the DB
+$query = 'INSERT INTO note(date, content, course_id) VALUES(:date, :content, :course_id)';
+$statement = $db->prepare($query);
+$statement->bindValue(':date', $date, PDO::PARAM_STR);
+$statement->bindValue(':content', $content, PDO::PARAM_STR);
+$statement->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+$result = $statement->execute();
+
+echo "$result";
+
 ?>
