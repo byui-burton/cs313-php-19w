c6aa1257107fbe86e0aae1bd0606ab6f04b7d375
Scott Burton <burtons@byui.edu>
2019-02-14 11:04:22 -0700

    insert note page

--- web/06-notes/notes.php
+++ web/06-notes/notes.php
@@ -2,7 +2,7 @@
 require_once("dbconnect.php");
 $db = get_db();
 
-$course_id = $_GET["course_id"];
+$course_id = htmlspecialchars($_GET["course_id"]);
 
 // Get the Course from the DB
 $query = 'SELECT id, name, course_code FROM course WHERE id=:id';
@@ -25,8 +25,15 @@ $course = $statement->fetch(PDO::FETCH_ASSOC);
 <?php
     $course_name = $course['name'];
     $course_code = $course['course_code'];
-    echo "<h1>Notes for $course_name - $course_code</h1>";
+    echo "<h1>Notes for  $course_code - $course_name</h1>";
 ?>
+
+    <form action="insert_note.php" method="post">
+        <input type="date" name="date" /><br />
+        <input type="hidden" name="course_id" value="<?php echo $course_id; ?>">
+        <textarea name="content"></textarea><br />
+        <input type="submit" value="Insert Note">
+    </form>
     
 </body>
 </html>
